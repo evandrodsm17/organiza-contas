619d2bdb344584d4dd1d67635e22a41f0921d271
evandrodsm17 <evandrodsm17@users.noreply.github.com>
2026-07-17 11:50:48 -0300

    melhorias

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Console > Configurações do projeto > Seus apps > Aplicativo Web
 export const firebaseConfig = {
-  apiKey: "SUBSTITUA_PELA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.firebasestorage.app",
-  messagingSenderId: "SUBSTITUA_PELO_SENDER_ID",
-  appId: "SUBSTITUA_PELO_APP_ID"
+  apiKey: "AIzaSyAdF_RU5ztoY575u9Ie0Q4AnLTHCUyGzxI",
+  authDomain: "organiza-contas-76388.firebaseapp.com",
+  projectId: "organiza-contas-76388",
+  storageBucket: "organiza-contas-76388.firebasestorage.app",
+  messagingSenderId: "368147696651",
+  appId: "1:368147696651:web:973d8d7603237a16e3cb0e"
 };
 
 // Região usada pelas Cloud Functions incluídas no projeto.
